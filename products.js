b90eedde06b542f7e8c1ab6f772c46a24b2ef0b2
Bryce King <bryceeking5@gmail.com>
2024-06-01 14:47:40 -0400

    fixing images

--- products.js
+++ products.js
@@ -9,9 +9,9 @@ function Cookie(name, price, image, circImg, description, priceNum, id){
     this.priceInCents = priceNum * 100;
     this.id = id;
  }
- const chocChipCookieDough = new Cookie("Cookie Dough", "$12.04", '/images/cookiedough.png', "/images/cookiedoughcirc.png", "Our delicious OG chocolate chip cookie batter infused with movie theater chocolate chip cookie dough! ",12.04, 0);
- const chocChip = new Cookie("OG Triple Chocolate Chip", "$12.04", '/images/chocchip.png', "/images/chocchipcirc.png", "Keep it ORIGINAL! Our deluctable chocolate chip cookies stuffed with enormous amounts of semi-sweet chocolate chips. ", 12.04, 1);
- const smores = new Cookie("Cookie and Smores", "$12.04", '/images/smores.png', "/images/smorescirc.png", "This cookie saves all the hussle and bussle of making a smore and packs TEN TIMES the flavor!", 12.04, 2);
+ const chocChipCookieDough = new Cookie("Cookie Dough", "$12.04", '/public/images/cookiedough.png', "/images/cookiedoughcirc.png", "Our delicious OG chocolate chip cookie batter infused with movie theater chocolate chip cookie dough! ",12.04, 0);
+ const chocChip = new Cookie("OG Triple Chocolate Chip", "$12.04", '/public/images/chocchip.png', "/images/chocchipcirc.png", "Keep it ORIGINAL! Our deluctable chocolate chip cookies stuffed with enormous amounts of semi-sweet chocolate chips. ", 12.04, 1);
+ const smores = new Cookie("Cookie and Smores", "$12.04", '/public/images/smores.png', "/images/smorescirc.png", "This cookie saves all the hussle and bussle of making a smore and packs TEN TIMES the flavor!", 12.04, 2);
  const whiteChocChip = new Cookie("OG White Chocolate Chip", "$12.04", '/images/whitechoc.png', "/images/whitechoccirc.png", "White chocolate chip morsels infused into a flavorful cookie!", 12.04, 3);
  const mm = new Cookie("M&M", "$12.04", '/images/mm.png', "/images/mm.png", "Simple, sweet and to the point. M&M candies packed into our OG cookie batter.", 12.04, 4);
  const whiteReesesPB = new Cookie("White Chocolate PB Cup", "$12.04", "/images/reeses.png", "/images/reesescirc.png", "Reeses Peanut Butter Cups combined with our white chocolate chip cookie batter!", 12.04, 5);
